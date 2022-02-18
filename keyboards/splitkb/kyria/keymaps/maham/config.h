e518b65d385531d26fd46cc697729483312673b1
Martin Hammarbrink <martinhammarbrink@neuraldsp.com>
2022-02-18 22:18:59 +0100

    Trying to get the Kyria to work like my Planck EZ

---
+++ keyboards/splitkb/kyria/keymaps/maham/config.h
@@ -0,0 +1,44 @@
+/* Copyright 2019 Thomas Baart <thomas@splitkb.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define DEBOUNCING_DELAY 80
+
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLIGHT_ANIMATIONS
+#    define RGBLIGHT_HUE_STEP  8
+#    define RGBLIGHT_SAT_STEP  8
+#    define RGBLIGHT_VAL_STEP  8
+#    define RGBLIGHT_LIMIT_VAL 150
+#endif
+
+// Lets you roll mod-tap keys
+#define IGNORE_MOD_TAP_INTERRUPT
+#define HOLD_ON_OTHER_KEYPRESS
+
+// If you are using an Elite C rev3 on the slave side, uncomment the lines below:
+// #define SPLIT_USB_DETECT
+// #define NO_USB_STARTUP_CHECK
+
+// MAHAM
+//#define LEADER_PER_KEY_TIMING
+//#define LEADER_TIMEOUT 250
+#define MASTER_RIGHT
+//#define UNICODE_SELECTED_MODES UC_MAC
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+
---
+++ keyboards/splitkb/kyria/keymaps/maham/rules.mk
@@ -0,0 +1,11 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+
+# MAHAM
+# LEADER_ENABLE = yes
+# POINTING_DEVICE_ENABLE = pimoroni_trackball
+# SWAP_HANDS_ENABLE = yes
+# UNICODEMAP_ENABLE = yes
+EXTRAFLAGS += -flto
